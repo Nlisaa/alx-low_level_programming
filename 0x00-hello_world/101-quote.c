7b5845fae6840fc65f0164c42190a74e63217a53
Nlisaa <otiendelisa@gmail.com>
2023-09-21 06:36:32 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	ssize_t len = strlen(message);
+	ssize_t written = write(2, message, len);
+
+	if (written == len)
+		return 1;
+	else
+		return 0;
+}
+
