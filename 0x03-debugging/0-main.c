c9dc03b93168d4ba0668a7e02e588552a16e05e7
Nlisaa <otiendelisa@gmail.com>
2023-09-24 06:34:16 -0700

    Updates

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Test function for positive or negative
+ * Return: 0
+ */
+int main(void)
+{
+    int i = 0; // Initialize and assign a value to 'i'
+    positive_or_negative(i);
+    return (0); // Use a semicolon to end the statement, not a colon (:)
+}
+
