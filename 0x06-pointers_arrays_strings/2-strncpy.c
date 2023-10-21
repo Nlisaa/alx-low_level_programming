c1fbe773c85ac6001df0069984e79ffd3a22e30f
Nlisaa <otiendelisa@gmail.com>
2023-10-21 01:56:27 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,30 +1,24 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates two strings using at most n bytes from src
- * @dest: destination string
+ * _strncpy - copies a string
+ * @dest: destination buffer
  * @src: source string
- * @n: maximum number of bytes from src
- * Return: pointer to the resulting string
+ * @n: maximum number of characters to copy
+ * Return: a pointer to the destination buffer
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-        int i, j;
+    int i;
 
-        i = 0;
-        while (dest[i] != '\0')
-        {
-                i++;
-        }
+    for (i = 0; i < n && src[i] != '\0'; i++) {
+        dest[i] = src[i];
+    }
 
-        j = 0;
-        while (j < n && src[j] != '\0')
-        {
-                dest[i] = src[j];
-                i++;
-                j++;
-        }
-
+    while (i < n) {
         dest[i] = '\0';
-        return (dest);
+        i++;
+    }
+
+    return dest;
 }
