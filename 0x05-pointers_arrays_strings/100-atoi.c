f5ec62c570438b60589845ea71a973eed3ec581e
Nlisaa <otiendelisa@gmail.com>
2023-10-23 09:37:56 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,25 +1,26 @@
 #include "main.h"
+#include <limits.h>
 
 int _atoi(char *s)
 {
     int result = 0;
-    int sign = 1; // Default to positive
+    int sign = 1; 
 
     while (*s)
     {
         if (*s == '-')
         {
-            sign *= -1; // Handle negative sign
+            sign *= -1
         }
         else if (*s == '+')
         {
-            // Handle positive sign (optional)
+           
         }
         else if (*s >= '0' && *s <= '9')
         {
-            // Convert character to integer
+            
             int digit = *s - '0';
-            // Check for integer overflow
+            
             if (result > (INT_MAX - digit) / 10)
             {
                 if (sign == 1)
@@ -31,7 +32,7 @@ int _atoi(char *s)
         }
         else if (result != 0)
         {
-            break; // Stop if non-digit characters follow the number
+            break;
         }
         s++;
     }
