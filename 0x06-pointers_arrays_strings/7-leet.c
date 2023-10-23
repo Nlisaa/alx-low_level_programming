d6e9522edc3e5ab6ff2f290f3a0c749ef772a6c5
Nlisaa <otiendelisa@gmail.com>
2023-10-23 09:54:45 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,27 +1,41 @@
 #include "main.h"
 
 /**
- * leet - encodes a string into 1337
- * @str: input string
- * Return: a pointer to the modified string
+ * leet - Encodes a string into 1337
+ * @str: Input string
+ * Return: Pointer to the modified string
  */
 char *leet(char *str)
 {
-    char *leet_table = "aAeEoOtTlL";
-    char *leet_replacement = "44370171";
-    int i, j;
-
-    for (i = 0; str[i] != '\0'; i++)
+    int i = 0;
+    while (str[i] != '\0')
     {
-        for (j = 0; leet_table[j] != '\0'; j++)
+        switch (str[i])
         {
-            if (str[i] == leet_table[j])
-            {
-                str[i] = leet_replacement[j];
-                break; // Exit the inner loop when a match is found
-            }
+            case 'a':
+            case 'A':
+                str[i] = '4';
+                break;
+            case 'e':
+            case 'E':
+                str[i] = '3';
+                break;
+            case 'o':
+            case 'O':
+                str[i] = '0';
+                break;
+            case 't':
+            case 'T':
+                str[i] = '7';
+                break;
+            case 'l':
+            case 'L':
+                str[i] = '1';
+                break;
+            default:
+                break;
         }
+        i++;
     }
-
-    return str;
+    return (str);
 }
