319252eb350bdfdb08ce35c6286670e1da345ca1
Nlisaa <otiendelisa@gmail.com>
2023-10-24 09:55:34 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,24 +2,18 @@
 
 /**
  * print_number - Prints an integer.
- * @n: The integer to print.
+ * @n: The integer to be printed.
  */
 void print_number(int n)
 {
-    int divisor = 1;
-
     if (n < 0)
     {
         _putchar('-');
         n = -n;
     }
 
-    while ((n / divisor) >= 10)
-        divisor *= 10;
+    if (n / 10)
+        print_number(n / 10);
 
-    while (divisor > 0)
-    {
-        _putchar((n / divisor) % 10 + '0');
-        divisor /= 10;
-    }
+    _putchar((n % 10) + '0');
 }
